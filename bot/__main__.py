139bb818c6dbe96a04bd115a6cf89fe3c2886c8b
Ameeruddin Syed <ameeruddin.sd@gmail.com>
2022-05-12 17:14:10 +0530

    Changes

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,9 @@
 from os.path import join as os_path_join
 from pyrogram import errors
-from pyrogram import Client
-#from pyrogram import Message
+from pyrogram import client
+from pyrogram import message
 from pyrogram.handlers import MessageHandler
-from pyrogram import  Filters, CallbackQueryHandler
+from pyrogram import  filters, CallbackQueryHandler
 from bot import CONFIG, COMMAND, LOCAL, LOGGER, STATUS
 from bot.handlers import *
 import asyncio
